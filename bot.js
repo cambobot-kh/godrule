a18c848f0902d86b17c81a5be58a8c889af9826f
healong <37354105+healong@users.noreply.github.com>
2018-04-06 12:29:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,5 +23,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '>help') {
+    	message.reply('https://ibb.co/hSaWMc');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
