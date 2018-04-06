46ea66814cf50ba54f5a913c4f438ded7c87ec8e
healong <37354105+healong@users.noreply.github.com>
2018-04-06 11:26:18 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '>link') {
+    	message.channel.send('https://discord.gg/7mS9GEY');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '>invite') {
+    	message.reply('http://bit.ly/2H0g96c');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '>owner') {
+    	message.reply('💎HeaLOng💎#5881');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
